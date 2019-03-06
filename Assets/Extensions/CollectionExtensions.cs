7718ac9c07c69610b238346ae7dbee841ccadfd7
wawila <davidc@neon.fyi>
2019-03-05 16:34:20 -0800

    Collection<Tile> Extension: NextStep

--- Assets/Extensions/CollectionExtensions.cs
+++ Assets/Extensions/CollectionExtensions.cs
@@ -12,4 +12,15 @@ public static class Extensions {
         var index = random.Next(0, c.Count);
         return c.ElementAt(index);
     }
+
+    public static Tile NextStep(this List<Tile> path, Tile current, int amount = 1) 
+    {
+        var index = path.IndexOf(current);
+        if (index == -1) return null;
+        var next = index + amount;
+        if (path.Count < next) {
+            return path.LastOrDefault();
+        }
+        return  path.ElementAtOrDefault(index+amount);
+    }
 }
