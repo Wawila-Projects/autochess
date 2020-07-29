e89422909e241d9d9713eb11c778c9098a431fc3
David Chavarria <dvchavarriam@gmail.com>
2020-07-29 06:02:43 -0600

    2020 first run

--- Assets/Map System/MapCoordinator.cs
+++ Assets/Map System/MapCoordinator.cs
@@ -1,17 +1,20 @@
-using UnityEngine;
 using System.Collections.Generic;
-public enum MapShapes 
-{
-    Triangle, ReverseTrianlge, Hexagonal, Rectangular
+using UnityEngine;
+public enum MapShapes {
+    Triangle,
+    ReverseTrianlge,
+    Hexagonal,
+    Rectangular
 }
 
-public enum MapType
-{
-    PointyEven, PointyOdd, FlatEven, FlatOdd
+public enum MapType {
+    PointyEven,
+    PointyOdd,
+    FlatEven,
+    FlatOdd
 }
 
-public class MapCoordinator: MonoBehaviour 
-{
+public class MapCoordinator : MonoBehaviour {
     public static MapCoordinator Coordinator;
     public MapShapes Shape = MapShapes.Rectangular;
     public MapType Type = MapType.PointyEven;
@@ -22,66 +25,78 @@ public class MapCoordinator: MonoBehaviour
     public int Height;
     public GameObject Prefab;
 
-    void Awake() {
-        Map = new List<Tile>();
-        var map = new HashSet<Hex>();
+    public Tile Origin;
+    public Color Color;
+    public int Range;
 
-        switch(Shape) {
+    void Awake () {
+        Map = new List<Tile> ();
+        var map = new HashSet<Hex> ();
+
+        switch (Shape) {
             case MapShapes.Triangle:
-                map = MapConstructor.TriangleMap(Width);
+                map = MapConstructor.TriangleMap (Width);
                 break;
             case MapShapes.ReverseTrianlge:
-                map = MapConstructor.ReverseTriangleMap(Width);
+                map = MapConstructor.ReverseTriangleMap (Width);
                 break;
             case MapShapes.Rectangular:
-                map = MapConstructor.RectangularlMap(Width, Height);
+                map = MapConstructor.RectangularlMap (Width, Height);
                 break;
             case MapShapes.Hexagonal:
-                map = MapConstructor.HexagonalMap(Width);
+                map = MapConstructor.HexagonalMap (Width);
                 break;
         }
 
-        var obstacles = new List<Hex>() {
+        var obstacles = new List<Hex> () {
             // new Hex(1, 0 ,-1), new Hex(1,1,-2), new Hex(0,3,-3), new Hex(1,4,-5),
-            // new Hex(2,3,-5), new Hex(2,2,-4), new Hex(3,1,-4), new Hex(0,2,-2)
+            //  new Hex(2,2,-4), new Hex(3,1,-4), new Hex(0,2,-2)
         };
 
-        foreach(var hex in map) {
-            var prefab = GameObject.Instantiate(Prefab, hex, Quaternion.identity);
-            prefab.name = hex.ToString();
-            var tile = prefab.GetComponent<Tile>();
+        foreach (var hex in map) {
+            var prefab = GameObject.Instantiate (Prefab, hex, Quaternion.identity);
+            prefab.name = hex.ToString ();
+            var tile = prefab.GetComponent<Tile> ();
 
             if (tile == null) continue;
 
             var isEven = Type == MapType.PointyOdd || Type == MapType.FlatOdd || Shape != MapShapes.Rectangular;
             if (Type == MapType.FlatEven || Type == MapType.FlatOdd) {
-                tile.InitFlat(hex, isEven);
-            } else  {
-                tile.InitPointy(hex, isEven);
+                tile.InitFlat (hex, isEven);
+            } else {
+                tile.InitPointy (hex, isEven);
             }
-            Map.Add(tile);
-            prefab.transform.SetParent(transform);
+            Map.Add (tile);
+            prefab.transform.SetParent (transform);
 
-            if(!obstacles.Contains(hex)) continue;
-            prefab.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
-            prefab.transform.localScale  = new Vector3(1,1,0.3f);
-            var z = (1-prefab.transform.localScale.z) * 0.125f;
-            prefab.transform.position += new Vector3(0,0,z);
+            if (hex == new Hex (-1, 2, -1)) {
+                Origin = tile;
+            }
+
+            if (!obstacles.Contains (hex)) continue;
+            prefab.GetComponent<Renderer> ().material.SetColor ("_Color", Color.green);
+            prefab.transform.localScale = new Vector3 (1, 1, 0.3f);
+            var z = (1 - prefab.transform.localScale.z) * 0.125f;
+            prefab.transform.position += new Vector3 (0, 0, z);
             tile.isObstacle = true;
+
         }
 
-        Map.ForEach((T) => T.GetNeighbors());
-        transform.rotation = Quaternion.Euler(90,0,0);
+        Map.ForEach ((T) => T.GetNeighbors ());
+        transform.rotation = Quaternion.Euler (90, 0, 0);
         DoneShowing = true;
     }
 
-    void Start()
-    {
+    void Start () {
         Coordinator = this;
     }
-    void Update() 
-    {
-        if(!Input.GetKeyDown(KeyCode.Space)) return;
+    void Update () {
+        if (!Input.GetKeyDown (KeyCode.Space)) return;
+        var tiles = Origin.GetTilesInsideRange (Range);
+        foreach (var tile in tiles) {
+            tile.GetComponent<Renderer> ()?.material.SetColor ("_Color", Color.red);
+        }
+
         // var path = AStar.FindPath(Start, End);
         // foreach(var tile in path) {
         //     tile.GetComponent<Renderer>()?.material.SetColor("_Color", Color.red);
--- Assets/Map System/Tile.cs
+++ Assets/Map System/Tile.cs
@@ -1,10 +1,9 @@
-using UnityEngine;
-using UnityEditor;
 using System;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
-public class Tile: MonoBehaviour
-{
+public class Tile : MonoBehaviour {
     public Hex Hex;
     public Character Occupant;
     public List<Tile> Neighbors;
@@ -15,91 +14,93 @@ public class Tile: MonoBehaviour
     public bool IsOccupied => Occupant != null;
     private OffsetCoordinates _coordinates;
 
-    public void Init(Hex hex) {
-        isObstacle =  tag == "Obstacle";
+    public void Init (Hex hex) {
+        isObstacle = tag == "Obstacle";
         Hex = hex;
-        GetNeighbors();
+        GetNeighbors ();
     }
 
-    public void InitPointy(Hex hex, bool even = true) {
+    public void InitPointy (Hex hex, bool even = true) {
         Hex = hex;
         float x;
         if (even) {
-            _coordinates = OffsetCoordinates.EvenPointyOffset(hex);
-            x = WorldY % 2 == 0 ? Mathf.Sqrt(3f)*WorldX : 
-                                    Mathf.Sqrt(3f)*(WorldX-1)+(Mathf.Sqrt(3f)/2f);
+            _coordinates = OffsetCoordinates.EvenPointyOffset (hex);
+            x = WorldY % 2 == 0 ? Mathf.Sqrt (3f) * WorldX :
+                Mathf.Sqrt (3f) * (WorldX - 1) + (Mathf.Sqrt (3f) / 2f);
         } else {
-            _coordinates = OffsetCoordinates.OddPointyOffset(hex);
-            x = WorldY % 2 == 0 ? Mathf.Sqrt(3f)*WorldX : 
-                                    Mathf.Sqrt(3f)*(WorldX-1)+(Mathf.Sqrt(3f)/2f);
+            _coordinates = OffsetCoordinates.OddPointyOffset (hex);
+            x = WorldY % 2 == 0 ? Mathf.Sqrt (3f) * WorldX :
+                Mathf.Sqrt (3f) * (WorldX - 1) + (Mathf.Sqrt (3f) / 2f);
         }
-        var y = WorldX % 2 == 0 ? 3f*WorldY/2f : 3f*WorldY;
-        transform.position = new Vector3(x, 3f*WorldY/2f);
+        var y = WorldX % 2 == 0 ? 3f * WorldY / 2f : 3f * WorldY;
+        transform.position = new Vector3 (x, 3f * WorldY / 2f);
     }
 
-    public void InitFlat(Hex hex, bool even = true) {
+    public void InitFlat (Hex hex, bool even = true) {
         Hex = hex;
         //gameObject.transform.rotation = Quaternion.Euler(0,0,30);
         if (even) {
-            _coordinates = OffsetCoordinates.EvenFlatOffset(hex);
+            _coordinates = OffsetCoordinates.EvenFlatOffset (hex);
         } else {
-            _coordinates = OffsetCoordinates.OddFlatOffset(hex);
+            _coordinates = OffsetCoordinates.OddFlatOffset (hex);
         }
-        var x = WorldY % 2 == 0 ? Mathf.Sqrt(3f)*WorldX : Mathf.Sqrt(3f)*(WorldX-1)+(Mathf.Sqrt(3f)/2f);
-        var y = WorldX % 2 == 0 ? 3f*WorldY/2f : 3f*WorldY;
-        transform.position = new Vector3(x, 3f*WorldY/2f);
+        var x = WorldY % 2 == 0 ? Mathf.Sqrt (3f) * WorldX : Mathf.Sqrt (3f) * (WorldX - 1) + (Mathf.Sqrt (3f) / 2f);
+        var y = WorldX % 2 == 0 ? 3f * WorldY / 2f : 3f * WorldY;
+        transform.position = new Vector3 (x, 3f * WorldY / 2f);
     }
 
-    public void GetNeighbors() {
-        Neighbors = new List<Tile>();
-        foreach(var hex in Hex.Neighbors) {
-            var go = GameObject.Find(hex.ToString());
+    public void GetNeighbors () {
+        Neighbors = new List<Tile> ();
+        foreach (var hex in Hex.Neighbors) {
+            var go = GameObject.Find (hex.ToString ());
             if (go != null)
-                Neighbors.Add(go.GetComponent<Tile>());
+                Neighbors.Add (go.GetComponent<Tile> ());
         }
     }
 
-    public int GetDistance(Tile other) {
-        return Hex.GetDistance(other.Hex);
+    public int GetDistance (Tile other) {
+        return Hex.GetDistance (other.Hex);
     }
 
-    public int GetDistance(Hex other) {
-        return Hex.GetDistance(other);
+    public int GetDistance (Hex other) {
+        return Hex.GetDistance (other);
     }
 
-    public List<Tile> GetTilesAtDistance(int distance) {
-        if (distance == 1) 
-        {
+    public List<Tile> GetTilesAtDistance (int distance) {
+        if (distance == 1) {
             return Neighbors;
         }
-        
-        var tiles = new List<Tile>();
-        foreach (var tile in MapCoordinator.Coordinator.Map)
-        {   
+
+        var tiles = new List<Tile> ();
+        foreach (var tile in MapCoordinator.Coordinator.Map) {
             if (tiles.Count == 6) break;
-            if (tile == this || tile.GetDistance(Hex) != distance) continue;
-            tiles.Add(tile);
+            if (tile == this || tile.GetDistance (Hex) != distance) continue;
+            tiles.Add (tile);
         }
         return tiles;
     }
 
-    public List<Tile> GetTilesInsideRange(int range) {
-        if (range == 1) 
-        {
+    public List<Tile> GetTilesInsideRange (int range) {
+        if (range == 1) {
             return Neighbors;
         }
 
-        var tiles = new List<Tile>();
-        foreach (var tile in MapCoordinator.Coordinator.Map)
-        {   
+        var tiles = new List<Tile> ();
+        foreach (var tile in MapCoordinator.Coordinator.Map) {
             if (tile == this) continue;
-            var checkX = Math.Abs(tile.Hex.X) <= range;
-            var checkY = Math.Abs(tile.Hex.Y) <= range;
-            var checkZ = Math.Abs(tile.Hex.Z) <= range;
+            var checkX = (tile.Hex.X) <= range + Hex.X &&
+                (tile.Hex.X) >= Hex.X -range;
+            var checkY = (tile.Hex.Y) <= range + Hex.Y &&
+                (tile.Hex.Y) >= Hex.Y - range;
+            var checkZ = (tile.Hex.Z) <= range + Hex.Z &&
+                (tile.Hex.Z) >= Hex.Z - range;
+
+            // var checkX = Math.Abs (tile.Hex.X) <= range;
+            // var checkY = Math.Abs (tile.Hex.Y) <= range;
+            // var checkZ = Math.Abs (tile.Hex.Z) <= range;
             var validHex = (tile.Hex.X + tile.Hex.Y + tile.Hex.Z) == 0;
-            if (checkX && checkY && checkZ && validHex) 
-            {
-                tiles.Add(tile);
+            if (checkX && checkY && checkZ && validHex) {
+                tiles.Add (tile);
             }
         }
         return tiles;
